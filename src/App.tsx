8107bb6725ebdd4a456c46f78787a121ebce8e50
agatha <agathakiriyok05@gmail.com>
2026-03-28 22:13:14 +0300

    Initial commit: create React + TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1> Movie Search</h1>
+      <p>Скоро тут будет поиск фильмов!</p>
+    </div>
+  )
+}
+
+export default App
